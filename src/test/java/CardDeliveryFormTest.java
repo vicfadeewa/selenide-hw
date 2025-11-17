9bfefd727a57160a7da5b6978b8a5b8c34642ab7
Victoriya Fadeeva <vic.f4deewa@gmail.com>
2025-11-17 10:38:47 +0300

    исправления в CardDeliveryFormTest

--- src/test/java/CardDeliveryFormTest.java
+++ src/test/java/CardDeliveryFormTest.java
@@ -1,6 +1,5 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -10,8 +9,6 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CardDeliveryFormTest {
@@ -19,19 +16,19 @@ public class CardDeliveryFormTest {
     void setUp() {
         Selenide.open("http://localhost:9999");
     }
-    @AfterEach
-    void tearDown() {
-        Selenide.closeWebDriver();
-    }
+//    @AfterEach
+//    void tearDown() {
+//        Selenide.closeWebDriver();
+//    }
 
-    private String generateDate(long addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    private String generateDate() {
+        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     void shouldSubmitFormSuccessfully() {
         $("[data-test-id='city'] input").setValue("Иркутск");
-        String planningDate = generateDate(4, "dd.MM.yyyy");
+        String planningDate = generateDate();
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE).setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Сидоров-Петров Игнат");
         $("[data-test-id='phone'] input").setValue("+79991234567");
